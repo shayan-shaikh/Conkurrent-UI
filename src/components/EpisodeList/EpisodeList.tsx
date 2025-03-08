04ef08b66fc18a953ad349fea0029de2bef9ae90
shayan-techolution <shayan.shaikh@techolution.com>
2025-03-09 02:23:57 +0530

    refactor: remove duplicacy with view all

--- src/components/EpisodeList/EpisodeList.tsx
+++ src/components/EpisodeList/EpisodeList.tsx
@@ -111,17 +111,6 @@ const EpisodeList = () => {
           ))}
         </div>
         
-        {/* <div className="text-center mt-12">
-          <a 
-            href="#" 
-            className="inline-flex items-center px-6 py-3 border border-purple-500 text-purple-500 rounded-full hover:bg-purple-500 hover:text-white transition-colors"
-          >
-            View All Episodes
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-            </svg>
-          </a>
-        </div> */}
       </div>
     </section>
   );
